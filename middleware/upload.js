6410192ffed58fe51e0c9f07e4d03fd9bac369de
AdriyKotsur <kotsurandriy@gmail.com>
2020-09-28 22:33:14 +0300

    aws files upload

--- middleware/upload.js
+++ middleware/upload.js
@@ -1,21 +1,33 @@
+require('dotenv').config();
+const aws = require('aws-sdk');
 const multer = require('multer');
-const jimp = require('jimp');
-const fs = require('fs');
+const multerS3 = require('multer-s3');
 const path = require('path');
 
-const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        const productId = req.params.id;
-        const folder = `./uploads/images/products/${productId}`
-        fs.access(folder, fs.constants.F_OK, () => {
-            if (!fs.constants.F_OK) {
-                return fs.mkdir(folder, {recursive: true}, error => cb(error, folder));
-            }
-            return cb(null, folder)
+const s3Config = new aws.S3({
+    accessKeyId: process.env.AWS_IAM_USER_KEY,
+    secretAccessKey: process.env.AWS_IAM_USER_SECRET,
+    region: process.env.AWS_REGION
+});
+
+const fileFilter = function (req, file, cb) {
+    if (!file.originalname.match(/\.(jpg|jpeg|png|svg)$/)) {
+        return cb(new Error('Please upload an image'))
+    }
+    cb(undefined, true)
+};
+
+const multerS3Config = multerS3({
+    s3: s3Config,
+    acl: 'public-read',
+    bucket: process.env.AWS_BUCKET_NAME,
+    metadata: function (req, file, cb) {
+        cb(null, {
+            fieldName: file.fieldname
         })
     },
-    filename: function (req, file, cb) {
-        cb(null, "product-" + Date.now() + path.extname(file.originalname));
+    key: function (req, file, cb) {
+        cb(null, 'product-' + Date.now() + path.extname(file.originalname))
     }
 });
 
@@ -23,13 +35,8 @@ const upload = multer({
     limits: {
         fileSize: 20000000
     },
-    fileFilter(req, file, cb) {
-        if (!file.originalname.match(/\.(jpg|jpeg|png|svg)$/)) {
-            return cb(new Error('Please upload an image'))
-        }
-        cb(undefined, true)
-    },
-    storage: storage
+    fileFilter: fileFilter,
+    storage: multerS3Config
 });
 
 const uploadFiles = upload.array('images', 10);
@@ -48,28 +55,7 @@ const uploadImages = (req, res, next) => {
     })
 };
 
-const optimizeImages = async (req, res, next) => {
-    await Promise.all(
-        req.files.map(async file => {
-            const image = await jimp.read(file.path);
-            const widthProps = image.bitmap.width;
-            const heightProps = image.bitmap.height;
-            if (widthProps < heightProps) {
-                await image.resize(400, 800);
-                await image.quality(70);
-                await image.writeAsync(file.path);
-            } else {
-                await image.resize(1200, 800);
-                await image.quality(70);
-                await image.writeAsync(file.path);
-            }
-        })
-    )
-
-    next();
-};
 
 module.exports = {
-    uploadImages,
-    optimizeImages
+    uploadImages
 };
--- routes/products.js
+++ routes/products.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const Product = require('../models/products');
 const router = express.Router();
-const fs = require('fs');
 const upload = require('../middleware/upload');
 
 /* Get Products */
@@ -35,15 +34,16 @@ router.post('/products', async function (req, res, next) {
   }
 
 });
+
 /* Add images */
-router.patch('/products/image/:id', upload.uploadImages, upload.optimizeImages, async function (req, res, next) {
+router.put('/products/image/:id', upload.uploadImages, async function (req, res, next) {
 
   const imgArr = [];
   const productId = req.params.id;
   console.log(req.files);
   
   req.files.map((file) => {
-    imgArr.push('/' + file.path);
+    imgArr.push(file.location); 
   });
   console.log(imgArr);
   
@@ -62,82 +62,9 @@ router.patch('/products/image/:id', upload.uploadImages, upload.optimizeImages, 
   });
 });
 
-/* FILES MANAGEING */
-router.get('/products/listAllFiles/:id', function (req, res, next) {
-  const folderId = req.params.id;
-  const files = fs.readdirSync(`./uploads/images/products/${folderId}`);
-
-  if (!files) {
-    res.status(404).send('Empty folder');
-  }
-  res.json(files);
-});
-
-router.get('/products/deleteAllFiles/:id', function (req, res, next) {
-  const folderId = req.params.id;
-  fs.readdir(`./uploads/images/products/${folderId}`, function (err, items) {
-    console.log(items);
-
-    items.forEach(function (item) {
-      console.log(item);
-      fs.unlinkSync(`./uploads/images/products/${folderId}/${item}`);
-    })
-
-    res.status(204).send('Files deleted');
-  });
-});
-
-/* UPDATE */
-router.patch('/products/:id', async function (req, res, next) {
-  const updates = Object.keys(req.body);
-  const allowUpdates = ['productTitle', 'productType', 'productDescription'];
-  const isValidOperation = updates.every((update) => allowUpdates.includes(update));
-
-  if (!isValidOperation) {
-    return res.status(400).send({
-      error: 'Invalid updates'
-    })
-  }
-
-  try {
-    const product = await Product.findOne({
-      _id: req.params.id
-    });
-
-    if (!product) {
-      return res.status(404).send();
-    }
-
-    updates.forEach((update) => product[update] = req.body[update])
-    await product.save()
-    res.send(product)
-  } catch {
-    res.status(400).send(e)
-  }
-});
-/* DELETE */
+/* Delete product */
 router.delete('/products/:id', async function (req, res, next) {
   const productId = req.params.id;
-
-  const removeFolder = function (folderPath) {
-    if (fs.existsSync(folderPath)) {
-      console.log(folderPath);
-
-      fs.readdirSync(folderPath).forEach(function (file) {
-        const currentPath = folderPath + '/' + file;
-        console.log(currentPath);
-
-        if (fs.lstatSync(currentPath).isDirectory()) {
-          removeFolder(currentPath);
-        } else {
-          fs.unlinkSync(currentPath);
-        }
-      });
-      fs.rmdirSync(folderPath);
-    };
-  };
-  removeFolder(`./uploads/images/products/${productId}`);
-
 
   try {
     const product = await Product.findOneAndDelete({
@@ -154,6 +81,7 @@ router.delete('/products/:id', async function (req, res, next) {
   }
 });
 
+/* Delete products */
 router.delete('/products/', async function (req, res, next) {
   const filter = {};
   try {
